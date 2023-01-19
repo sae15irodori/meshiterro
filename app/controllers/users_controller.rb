1919801ed89c4f2a49575774bf1e6d163aa54b0d
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 00:42:29 +0000

    Finish all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
-  
+
   def show
     @user = User.find(params[:id])#特定IDユーザーのレコード取得
     @post_images = @user.post_images.page(params[:page])#そのﾕｰｻﾞｰに関連づいた投稿内容取得
@@ -15,13 +15,13 @@ class UsersController < ApplicationController
     @user.update(user_params)#データの更新
     redirect_to user_path(@user.id)#詳細ﾍﾟｰｼﾞへﾘﾀﾞｲﾚｸﾄ(特定IDのページ)
   end
-  
-  
+
+
   private
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
   def is_matching_login_user
     user_id = params[:id].to_i
     unless user_id == current_user.id
